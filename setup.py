a9803a1fe3b5e7c08f45bfe715bcd2ea3d0f7f16
thomask156 <thomask156@gmail.com>
2018-08-15 18:15:54 -0400

    Added a setup.py file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Pickup Farming',
+    version='0.8dev',
+    packages=['pynput', 'pyscreenshot', 'pywin32', 'PyQt5'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
